b0716a89061b9d98ec1c8efc550cfb3c834b171b
Arron Hartley <arronhartley@gmail.com>
2013-08-07 12:16:39 +0100

    Added some useful Android build framework bits and bobs including libpng for resource loading.

---
+++ Build/Android/jni/Android.mk
@@ -0,0 +1,4 @@
+MY_DIR := $(call my-dir)
+
+include $(MY_DIR)/libpng/Android.mk
+include $(MY_DIR)/GravityWarsRedux/Android.mk
---
+++ Build/Android/src/uk/co/forgreatjustice/GravityWarsRedux/GravityWarsRedux.java
@@ -0,0 +1,13 @@
+package uk.co.forgreatjustice.GravityWarsRedux;
+
+import android.app.NativeActivity;
+import android.os.Bundle;
+
+public class GravityWarsRedux extends NativeActivity
+{
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
